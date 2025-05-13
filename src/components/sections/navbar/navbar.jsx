cf3a5843e71e0129707ed6c5c28a58df8acc2baf
Nikita <yanakiiev@gmail.com>
2025-05-13 02:10:16 +0200

    add build

--- src/components/sections/navbar/navbar.jsx
+++ src/components/sections/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { RiBookShelfFill } from "react-icons/ri";
 import { FaTrophy } from "react-icons/fa";
@@ -8,15 +7,27 @@ import { FaTrophy } from "react-icons/fa";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <nav className="navbar">
-      <Link to="/deutsch-lernen" className="navbar__home navbar__icons">
+      <Link
+        to="/deutsch-lernen"
+        className={`navbar__home navbar__icons ${currentPath === "/deutsch-lernen" ? "active" : ""}`}
+      >
         <AiFillHome />
       </Link>
-      <Link to="/dictionary" className="navbar__books navbar__icons">
+      <Link
+        to="/dictionary"
+        className={`navbar__books navbar__icons ${currentPath === "/dictionary" ? "active" : ""}`}
+      >
         <RiBookShelfFill />
       </Link>
-      <Link to="/achievements" className="navbar__achievements navbar__icons">
+      <Link
+        to="/achievements"
+        className={`navbar__achievements navbar__icons ${currentPath === "/achievements" ? "active" : ""}`}
+      >
         <FaTrophy />
       </Link>
     </nav>
